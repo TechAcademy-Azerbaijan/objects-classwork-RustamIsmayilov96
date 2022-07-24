b47cb692ec7bb1528aa98aa9bfa707e467a65523
RustamIsmayilov96 <106178348+RustamIsmayilov96@users.noreply.github.com>
2022-07-24 22:29:35 +0300

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -1,14 +1,15 @@
 let figure = {
-    title: "rectangle",
-    length: 10,
-    width: 6,
-    area : function() {
-        // Write code here
-    },
-    perimeter: function(){
-        // Write code here
-    }
-  };
+  title: "rectangle",
+  length: 10,
+  width: 6,
+  area : function() {
+      return figure.length*figure.width
+  },
+  perimeter: function(){
+      // Write code here
+      return 2*(figure.length+figure.width)
+  }
+};
 
-  console.log(figure.area());
-  console.log(figure.perimeter());   
+console.log(figure.area());
+console.log(figure.perimeter()); 
